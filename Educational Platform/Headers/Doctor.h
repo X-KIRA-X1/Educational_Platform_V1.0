50180d018e046c32ccff6ae1878f8b9fadf399d8
Saeed haroun <saeedharoun59@gmail.com>
2025-10-02 14:56:55 +0300

    Update Doctor.h

--- Educational Platform/Headers/Doctor.h
+++ Educational Platform/Headers/Doctor.h
@@ -14,20 +14,15 @@ private:
     vector <string> Courses;
 public:
     Doctor (){}
-    Doctor(string Name , string Mail , string Password , vector <string> Courses)
-    {
-        this -> Name = Name;
-        this -> Mail = Mail;
-        this -> Password = Password;
-        this -> Courses = Courses;
-    }
-    void Print()
+    Doctor(const string &Name ,const string &Mail ,const string &Password ,const vector <string> &Courses)
+        :Name(Name) , Mail(Mail) , Password(Password) , Courses(Courses){}
+    void Print() const 
     {
         cout << Name << " " << Mail << " "  << Password << " " ;
         for (auto i : Courses) cout << i << " ";
         cout << '\n';
     }
-    string GetPassword () {return Password;}
+    const string &GetPassword () {return Password;}
     void DoctorPortal(vector <AssignmentAnswer> &AssignmentsAnswers);
     void AddAssignment(string ChosenCourse , vector <AssignmentAnswer> &AssignmentsAnswers);
     void ShowAssignmentsAnswers(string ChosenCourse , vector <AssignmentAnswer> &AssignmentsAnswers);
