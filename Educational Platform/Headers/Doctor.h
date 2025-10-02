a1985136806a2249a75ff0a87cc56973e4fe81a3
Saeed haroun <saeedharoun59@gmail.com>
2025-10-02 15:19:17 +0300

    Update Doctor.h

--- Educational Platform/Headers/Doctor.h
+++ Educational Platform/Headers/Doctor.h
@@ -16,7 +16,7 @@ public:
     Doctor (){}
     Doctor(const string &Name ,const string &Mail ,const string &Password ,const vector <string> &Courses)
         :Name(Name) , Mail(Mail) , Password(Password) , Courses(Courses){}
-    void Print() const 
+    void Print() const
     {
         cout << Name << " " << Mail << " "  << Password << " " ;
         for (auto i : Courses) cout << i << " ";
@@ -24,11 +24,11 @@ public:
     }
     const string &GetPassword () {return Password;}
     void DoctorPortal(vector <AssignmentAnswer> &AssignmentsAnswers);
-    void AddAssignment(string ChosenCourse , vector <AssignmentAnswer> &AssignmentsAnswers);
-    void ShowAssignmentsAnswers(string ChosenCourse , vector <AssignmentAnswer> &AssignmentsAnswers);
-    void AddLectureLink(string ChosenCourse , vector <AssignmentAnswer> &AssignmentsAnswers);
-    void AddGrade (string ChosenCourse , vector <AssignmentAnswer> &CourseAssignmentsAnswers ,  vector <AssignmentAnswer> &AssignmentsAnswers);
-    void ShowGrades (string ChosenCourse , vector <AssignmentAnswer> &AssignmentsAnswers);
+    void AddAssignment(const string &ChosenCourse , vector <AssignmentAnswer> &AssignmentsAnswers);
+    void ShowAssignmentsAnswers(const string &ChosenCourse , vector <AssignmentAnswer> &AssignmentsAnswers);
+    void AddLectureLink(const string &ChosenCourse , vector <AssignmentAnswer> &AssignmentsAnswers);
+    void AddGrade (const string &ChosenCourse , vector <AssignmentAnswer> &CourseAssignmentsAnswers ,  vector <AssignmentAnswer> &AssignmentsAnswers);
+    void ShowGrades (const string &ChosenCourse , vector <AssignmentAnswer> &AssignmentsAnswers);
 
 };
 
